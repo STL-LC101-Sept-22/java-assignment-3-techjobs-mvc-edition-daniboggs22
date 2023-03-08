a710751bf61a302e296280d3d5a398c5beb142e2
Daneille Boggs <daniboggs22@gmail.com>
2023-03-07 20:52:07 -0600

    fixed the extra column in list.html so 'View All' is in the 'All' column.

--- src/main/java/org/launchcode/techjobs/mvc/controllers/ListController.java
+++ src/main/java/org/launchcode/techjobs/mvc/controllers/ListController.java
@@ -29,6 +29,7 @@ public class ListController {
         columnChoices.put("positionType", "Position Type");
         columnChoices.put("coreCompetency", "Skill");
 
+        tableChoices.put("all", "View All");
         tableChoices.put("employer", JobData.getAllEmployers());
         tableChoices.put("location", JobData.getAllLocations());
         tableChoices.put("positionType", JobData.getAllPositionTypes());
--- src/main/java/org/launchcode/techjobs/mvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/techjobs/mvc/controllers/SearchController.java
@@ -1,9 +1,15 @@
 package org.launchcode.techjobs.mvc.controllers;
 
+import org.launchcode.techjobs.mvc.models.Job;
+import org.launchcode.techjobs.mvc.models.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
 
 import static org.launchcode.techjobs.mvc.controllers.ListController.columnChoices;
 
@@ -22,5 +28,19 @@ public class SearchController {
     }
 
     // TODO #3 - Create a handler to process a search request and render the updated search view.
+//    @PostMapping("search/results")
+//    public String displaySearchResults(Model model, @RequestParam String column, @RequestParam(required = false)
+//    String key){
+//        ArrayList<Job> jobs;
+//        if(column.equals("all")){
+//            jobs = JobData.findAll();
+//            model.addAttribute("title", "All Jobs");
+//
+//        }
+//        model.addAttribute("jobs", jobs);
+//
+//        return "search";
+//    }
+
 
 }
